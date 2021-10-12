d0617b4d7d19df2e261f05e460e3c708f4dfb683
Lijuet <sleechae@gmail.com>
2021-10-12 18:07:30 +0900

    [1012] login front

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="../assets/img/logo.png" />
     <button @click="removeToken">Logout</button>
     <button @click="listAutoMatchResult">Auto Matching</button>
     <div>
